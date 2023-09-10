91c56cd20278f81f5fc393d2329ac369cbb1bb75
fixedcow <puren37@gmail.com>
2023-09-10 16:14:27 +0900

    feat: 월드매니저 버그 수정
    - 아무튼 고침 ㅋㅋ

--- Assets/Scripts/Utils/OreData.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(menuName ="SO/Ore Data", fileName = "New Ore data")]
-public class OreData : WorldObjectData
-{
-	public string race;
-}
