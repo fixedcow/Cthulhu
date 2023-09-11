7c21c2b3fa07c061f1a4f94dc484c43ee4ac9702
YuTaehwan <ytw0113@gmail.com>
2023-09-12 06:31:23 +0900

    feat: 공물 ui 완성

--- Assets/Scripts/Offering/OfferCardUITest.cs
+++ Assets/Scripts/Offering/OfferCardUITest.cs
@@ -1,21 +1,66 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using DG.Tweening;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 namespace TH.Core {
 
-public class OfferCardUITest : MonoBehaviour
+public class OfferCardUITest : MonoBehaviour, IPointerClickHandler, IPointerEnterHandler, IPointerExitHandler
 {
     #region PublicVariables
 	#endregion
 
 	#region PrivateVariables
+	private Action _action;
+	private Action<int> _onClick;
+	private int _index;
+	private bool _isClicked;
 	#endregion
 
 	#region PublicMethod
-	public void SetText(string title, string description) {
+	public void SetText(string title, string description, Action action, Action<int> onClick, int index) {
+		_action = action;
+		_onClick = onClick;
+		_index = index;
+		_isClicked = false;
 		transform.Find("CardTitle").GetComponent<TMPro.TextMeshProUGUI>().text = title;
 		transform.Find("CardMiddleTitle").GetComponent<TMPro.TextMeshProUGUI>().text = description;
+		GetComponent<Image>().material.SetFloat("_FadeAmount", 0);
+		GetComponent<Image>().material.SetFloat("_InnerOutlineThickness", 0);
+	}
+
+	public void OnPointerClick(PointerEventData eventData)
+	{
+		if (_isClicked) return;
+		_isClicked = true;
+		_onClick(_index);
+		_action();
+		Select();
+	}
+
+	public void OnPointerEnter(PointerEventData eventData)
+	{
+		GetComponent<RectTransform>().localScale = Vector3.one;
+		GetComponent<RectTransform>().DOScale(1.1f, 0.1f);
+	}
+
+	public void OnPointerExit(PointerEventData eventData)
+	{
+		GetComponent<RectTransform>().localScale = Vector3.one * 1.1f;
+		GetComponent<RectTransform>().DOScale(1, 0.1f);
+	}
+
+	public void Unselect() {
+		DOTween.To(() => GetComponent<Image>().material.GetFloat("_FadeAmount"), x => GetComponent<Image>().material.SetFloat("_FadeAmount", x), 1, 1f);
+		transform.Find("CardTitle").GetComponent<TMPro.TextMeshProUGUI>().text = "";
+		transform.Find("CardMiddleTitle").GetComponent<TMPro.TextMeshProUGUI>().text = "";
+	}
+
+	public void Select () {
+		GetComponent<Image>().material.SetFloat("_InnerOutlineThickness", 1);
 	}
 	#endregion
     
--- Assets/Scripts/Offering/OfferUITest.cs
+++ Assets/Scripts/Offering/OfferUITest.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using Sirenix.OdinInspector;
+using Unity.VisualScripting;
 using UnityEngine;
 
 namespace TH.Core {
@@ -20,13 +21,31 @@ public class OfferUITest : MonoBehaviour
 		for (int i = 0; i < _cardUI.Length; i++) {
 			_cardUI[i].SetText(
 				OfferingManager.Instance.OfferingData.GetTierPresents(1)[i].GetName(), 
-				OfferingManager.Instance.OfferingData.GetTierPresents(1)[i].GetDescription()
+				OfferingManager.Instance.OfferingData.GetTierPresents(1)[i].GetDescription(),
+				() => OfferingManager.Instance.OfferingData.GetTierPresents(1)[2].InvokePresent(),
+				UnselectOther,
+				i
 			);
 		}
+	}
+
+	public void UnselectOther(int idx) {
+		for (int i = 0; i < _cardUI.Length; i++) {
+			if (i != idx) {
+				_cardUI[i].Unselect();
+			}
+		}
+	}
+
+	public void Deactivate() {
+		gameObject.SetActive(false);
 	}
 	#endregion
     
 	#region PrivateMethod
+	private void OnEnable() {
+		SetCardUI();
+	}
 	#endregion
 }
 
